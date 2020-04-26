d85d98c4cebce3ad76977e4901b7b1a3cea6114b
Gilles Fabre <gilles.fabre@altran.com>
2020-04-26 16:36:47 +0200

    First run with handlebars

--- src/main.js
+++ src/main.js
@@ -3,17 +3,20 @@ exports.__esModule = true;
 var ts = require("typescript");
 var fse = require("fs-extra");
 var walker_1 = require("./features/walker");
+var report_service_1 = require("./features/report.service");
 var appRootPath = require('app-root-path');
 var Main = /** @class */ (function () {
     function Main() {
-        this.source = __dirname;
         this.appRoot = appRootPath.toString(); // Root of the app
     }
     Main.prototype.process = function () {
-        console.log("SOURCE " + this.source);
         console.log("appRoot " + this.appRoot);
-        console.log();
+        var reportService = new report_service_1.ReportService();
+        reportService.generate();
         var sourceFile = ts.createSourceFile('methods.mock.ts', fse.readFileSync(this.appRoot + "/src/mocks/methods.mock.ts", 'utf8'), ts.ScriptTarget.Latest);
+        // this.reportTemplate = Handlebars.compile(fse.readFileSync(`${this.appRoot}/src/mocks/methods.mock.ts`, 'utf8'));
+        // console.log('template ', this.reportTemplate);
+        // this.reportTemplate({score: 'zzz'});
         var walker = new walker_1.Walker(sourceFile);
         walker.walk();
     };
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
 import * as ts from 'typescript';
 import * as fse from 'fs-extra';
+import * as Handlebars from 'handlebars';
 import { Walker } from './features/walker';
+import { ReportService } from './features/report.service';
 
 const appRootPath = require('app-root-path');
 
 export class Main {
 
-    private source = __dirname;
     private appRoot = appRootPath.toString();                   // Root of the app
+    private reportTemplate: HandlebarsTemplateDelegate;
 
     constructor() {}
 
     process(): void {
-        console.log(`SOURCE ${this.source}`);
         console.log(`appRoot ${this.appRoot}`);
-        console.log()
+        const reportService = new ReportService();
+        reportService.generate();
         const sourceFile = ts.createSourceFile('methods.mock.ts',
             fse.readFileSync(`${this.appRoot}/src/mocks/methods.mock.ts`, 'utf8'),
             ts.ScriptTarget.Latest);
+        // this.reportTemplate = Handlebars.compile(fse.readFileSync(`${this.appRoot}/src/mocks/methods.mock.ts`, 'utf8'));
+        // console.log('template ', this.reportTemplate);
+        // this.reportTemplate({score: 'zzz'});
         const walker = new Walker(sourceFile);
         walker.walk();
     }
