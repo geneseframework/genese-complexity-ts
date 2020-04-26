4c630643f1de80c536692a3f6cbbbe31931eeff7
Gilles Fabre <gilles.fabre@altran.com>
2020-04-26 19:33:18 +0200

    Cleaned code

---
+++ src/mocks/second.mock.ts
@@ -0,0 +1,32 @@
+class FirstMock {
+
+	constructor() {
+	}
+
+
+	sumOfPrimes(max: number): number {
+		let total = 0;
+		for (let i = 1; i < max; ++i) {
+			for (let j = 2; j < i; ++j) {
+				if (i % j === 0) {
+					console.log(`sumOfPrimes i = ${i} / j = ${j}`);
+				}
+			}
+			total += i;
+		}
+		return total;
+	}
+
+
+
+	getWords(numberOfWords: number) {
+		switch (numberOfWords) {
+			case 1:
+				return "one";
+			case 2:
+				return "a couple";
+			default:
+				return "lots";
+		}
+	}
+}
