2f0a95951f36765c5708171e65392d3e938c8a6c
Gilles Fabre <gilles.fabre@altran.com>
2020-04-26 18:14:37 +0200

    Added row handlebars

--- src/features/report.service.js
+++ src/features/report.service.js
@@ -20,13 +20,15 @@ var ReportService = /** @class */ (function () {
         else {
             fs.mkdirsSync(this.outDir);
         }
-        var text = eol.auto(fs.readFileSync(this.appRoot + "/src/templates/report.handlebars", 'utf-8'));
-        this.template = Handlebars.compile(text);
+        var rowTemplate = eol.auto(fs.readFileSync(this.appRoot + "/src/templates/row.handlebars", 'utf-8'));
+        Handlebars.registerPartial("analyse", rowTemplate);
+        var reportTemplate = eol.auto(fs.readFileSync(this.appRoot + "/src/templates/report.handlebars", 'utf-8'));
+        this.template = Handlebars.compile(reportTemplate);
         this.writeReport();
         console.log('REPORT GENERATED SUCCESSFULLY');
     };
     ReportService.prototype.writeReport = function () {
-        var ts = this.template({ score: 'aaa' });
+        var ts = this.template({ score: 'aaa', analyses: [{ filename: 'zzz' }] });
         fs.writeFileSync(this.outDir + "/report.html", ts, { encoding: 'utf-8' });
         fs.copyFileSync(this.appRoot + "/src/templates/report.css", this.outDir + "/report.css");
         fs.copyFileSync(this.appRoot + "/src/templates/prettify.css", this.outDir + "/prettify.css");
--- src/features/report.service.ts
+++ src/features/report.service.ts
@@ -25,15 +25,19 @@ export class ReportService {
         } else {
             fs.mkdirsSync(this.outDir);
         }
-        const text = eol.auto(fs.readFileSync(`${this.appRoot}/src/templates/report.handlebars`, 'utf-8'));
-        this.template = Handlebars.compile(text);
+        const rowTemplate = eol.auto(fs.readFileSync(`${this.appRoot}/src/templates/row.handlebars`, 'utf-8'));
+        Handlebars.registerPartial(
+            "analyse",
+        rowTemplate);
+        const reportTemplate = eol.auto(fs.readFileSync(`${this.appRoot}/src/templates/report.handlebars`, 'utf-8'));
+        this.template = Handlebars.compile(reportTemplate);
 
         this.writeReport();
         console.log('REPORT GENERATED SUCCESSFULLY');
     }
 
     private writeReport() {
-        const ts = this.template({score: 'aaa'});
+        const ts = this.template({score: 'aaa', analyses: [{filename: 'zzz'}]});
         fs.writeFileSync(`${this.outDir}/report.html`, ts, {encoding: 'utf-8'});
         fs.copyFileSync(`${this.appRoot}/src/templates/report.css`, `${this.outDir}/report.css`);
         fs.copyFileSync(`${this.appRoot}/src/templates/prettify.css`, `${this.outDir}/prettify.css`);
