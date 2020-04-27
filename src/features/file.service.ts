9eccd996d4345f8c080e8971a006330cf496761a
Genese <63037808+geneseframework@users.noreply.github.com>
2020-04-27 09:02:02 +0200

    Parse folders (#4)
    Co-authored-by: Gilles Fabre <gilles.fabre@altran.com>

--- src/features/file.service.ts
+++ src/features/file.service.ts
@@ -2,7 +2,7 @@
 import * as fs from 'fs-extra';
 const path = require("path")
 
-export function getFileName(filePath = ''): string {
+export function getFilename(filePath = ''): string {
     const splittedPath = filePath.split('/');
     return splittedPath[splittedPath.length - 1];
 }
@@ -11,15 +11,24 @@ export function getFileName(filePath = ''): string {
 export function getAllFiles(dirPath: string, arrayOfFiles?: string[]): string[] {
     const files = fs.readdirSync(dirPath)
 
-    arrayOfFiles = arrayOfFiles || []
+    arrayOfFiles = arrayOfFiles || [];
 
     files.forEach(function(file) {
         if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-            arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+            arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
         } else {
-            arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+            arrayOfFiles.push(`${dirPath}/${file}`);
         }
     })
+    return arrayOfFiles;
+}
 
-    return arrayOfFiles
+
+export function getTsFiles(dirPath: string) {
+    return getAllFiles(dirPath).filter(e => getExtension(e) === 'ts');
+}
+
+
+export function getExtension(filename: string): string {
+    return filename.split('.').pop();
 }
