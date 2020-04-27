7a6a89ef2b77fb9822e8c9cac25c343d3f8ab08d
Gilles Fabre <gilles.fabre@altran.com>
2020-04-27 22:34:52 +0200

    Added mocks

--- src/mocks/first.mock.js
+++ src/mocks/first.mock.js
@@ -1,19 +1,60 @@
 var FirstMock = /** @class */ (function () {
     function FirstMock() {
     }
-    FirstMock.prototype.sumOfPrimes = function (max) {
+    FirstMock.prototype.forforfor = function (max) {
         var total = 0;
         for (var i = 1; i < max; ++i) {
             for (var j = 2; j < i; ++j) {
-                if (i % j === 0) {
-                    console.log("sumOfPrimes i = " + i + " / j = " + j);
+                for (var k = 2; k < 10; ++k) {
+                    console.log("k = " + k);
                 }
             }
             total += i;
         }
         return total;
     };
-    FirstMock.prototype.getWords = function (numberOfWords) {
+    FirstMock.prototype.ififif = function (data) {
+        if (data > 2) {
+            if (data > 3) {
+                if (data > 4) {
+                    console.log('data > 4');
+                }
+            }
+        }
+    };
+    FirstMock.prototype.ifififelse = function (data) {
+        if (data > 2) {
+            if (data > 3) {
+                if (data > 4) {
+                    console.log('data > 4');
+                }
+                else {
+                    console.log('data <= 4');
+                }
+            }
+        }
+    };
+    FirstMock.prototype.ifAnd = function (a, b) {
+        if (a && b) {
+            console.log(a);
+        }
+    };
+    FirstMock.prototype.ifOr = function (a, b) {
+        if (a || b) {
+            console.log(a);
+        }
+    };
+    FirstMock.prototype.ifAndAnd = function (a, b, c) {
+        if (a && b && c) {
+            console.log(a);
+        }
+    };
+    FirstMock.prototype.ifAndOr = function (a, b, c) {
+        if (a && b || c) {
+            console.log(a);
+        }
+    };
+    FirstMock.prototype.switches = function (numberOfWords) {
         switch (numberOfWords) {
             case 1:
                 return "one";
--- src/mocks/first.mock.ts
+++ src/mocks/first.mock.ts
@@ -1,32 +1,85 @@
 class FirstMock {
 
-	constructor() {
-	}
+    constructor() {
+    }
 
 
-	sumOfPrimes(max: number): number {
-		let total = 0;
-		for (let i = 1; i < max; ++i) {
-			for (let j = 2; j < i; ++j) {
-				if (i % j === 0) {
-					console.log(`sumOfPrimes i = ${i} / j = ${j}`);
-				}
-			}
-			total += i;
-		}
-		return total;
-	}
+    forforfor(max: number): number {
+        let total = 0;
+        for (let i = 1; i < max; ++i) {
+            for (let j = 2; j < i; ++j) {
+                for (let k = 2; k < 10; ++k) {
+                    console.log(`k = ${k}`);
+                }
+            }
+            total += i;
+        }
+        return total;
+    }
+
+
+    ififif(data: number): void {
+        if (data > 2) {
+            if (data > 3) {
+                if (data > 4) {
+                    console.log('data > 4');
+                }
+            }
+        }
+    }
 
 
 
-	getWords(numberOfWords: number) {
-		switch (numberOfWords) {
-			case 1:
-				return "one";
-			case 2:
-				return "a couple";
-			default:
-				return "lots";
-		}
-	}
+    ifififelse(data: number): void {
+        if (data > 2) {
+            if (data > 3) {
+                if (data > 4) {
+                    console.log('data > 4');
+                } else {
+                    console.log('data <= 4');
+                }
+            }
+        }
+    }
+
+
+    ifAnd(a, b) {
+        if (a && b) {
+            console.log(a);
+        }
+    }
+
+
+    ifOr(a, b) {
+        if (a || b) {
+            console.log(a);
+        }
+    }
+
+
+    ifAndAnd(a, b, c) {
+        if (a && b && c) {
+            console.log(a);
+        }
+    }
+
+
+    ifAndOr(a, b, c) {
+        if (a && b || c) {
+            console.log(a);
+        }
+    }
+
+
+
+    switches(numberOfWords: number) {
+        switch (numberOfWords) {
+            case 1:
+                return "one";
+            case 2:
+                return "a couple";
+            default:
+                return "lots";
+        }
+    }
 }
