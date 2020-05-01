afc594db57e5ffb0e4487fa607fb2d3de7d5797b
Gilles Fabre <gilles.fabre@altran.com>
2020-05-01 17:25:34 +0200

    Added subfolders with ts files

---
+++ src/mocks/mock-subfolder-one/first.mock.ts
@@ -0,0 +1,100 @@
+import { Evaluation } from '../models/evaluation';
+
+class FirstMock {
+
+    constructor() {
+    }
+
+
+
+    ifElse(data): Evaluation {
+        if (data === 'a') {
+            data = 'b';
+        } else {
+            data = 'c';
+        }
+        return {cyclomaticValue: 2, cognitiveValue: 1};
+    }
+
+
+    ifAnd(a, b): Evaluation {
+        if (a && b) {
+            console.log(a);
+        }
+        return {cyclomaticValue: 3, cognitiveValue: 2};
+    }
+
+
+    ifOr(a, b): Evaluation {
+        if (a || b) {
+            console.log(a);
+        }
+        return {cyclomaticValue: 3, cognitiveValue: 2};
+    }
+
+
+    ifAndAnd(a, b, c): Evaluation {
+        if (a && b && c) {
+            console.log(a);
+        }
+        return {cyclomaticValue: 4, cognitiveValue: 2};
+    }
+
+
+    ifAndOr(a, b, c): Evaluation {
+        if (a && b || c) {
+            console.log(a);
+        }
+        return {cyclomaticValue: 4, cognitiveValue: 3};
+    }
+
+    ifIfIf(data: number): Evaluation {
+        if (data > 2) {
+            if (data > 3) {
+                if (data > 4) {
+                    console.log('data > 4');
+                }
+            }
+        }
+        return {cyclomaticValue: 4, cognitiveValue: 6};
+    }
+
+
+
+    ifIfIfElse(data: number): Evaluation {
+        if (data > 2) {
+            if (data > 3) {
+                if (data > 4) {
+                    console.log('data > 4');
+                } else {
+                    console.log('data <= 4');
+                }
+            }
+        }
+        return {cyclomaticValue: 4, cognitiveValue: 6};
+    }
+
+
+    ifAndAndOrAnd(a, b, c, d, e, f): Evaluation {
+        if (a && b && c || d && e && f) {
+            console.log(a);
+        }
+        return {cyclomaticValue: 7, cognitiveValue: 4};
+    }
+
+
+
+    switches(numberOfWords: number): Evaluation {
+        switch (numberOfWords) {
+            case 1:
+                console.log("one");
+                break;
+            case 2:
+                console.log("a couple");
+                break;
+            default:
+                console.log("lots");
+        }
+        return {cyclomaticValue: 3, cognitiveValue: 1};
+    }
+}
