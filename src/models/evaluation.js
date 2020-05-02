c00d7dccc571f4212aaec3ae12eedb69c786fa41
Gilles Fabre <gilles.fabre@altran.com>
2020-05-02 11:42:34 +0200

    Added evals for root files

--- src/models/evaluation.js
+++ src/models/evaluation.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var Evaluation = /** @class */ (function () {
     function Evaluation() {
         this.cognitiveValue = 0;
