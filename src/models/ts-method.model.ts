e4d79eb3d06883595630eece72560749b418a5b1
Gilles Fabre <gilles.fabre@altran.com>
2020-05-02 15:41:02 +0200

    Depth is correct for ifs

--- src/models/ts-method.model.ts
+++ src/models/ts-method.model.ts
@@ -1,7 +1,7 @@
 import * as ts from 'typescript';
-import * as utils from 'tsutils';
 import { TsFile } from './ts-file.model';
 import { Ast } from '../services/ast.service';
+import { ComplexityService as CS } from '../services/complexity.service';
 import { Evaluation } from './evaluation.model';
 import { TsBloc } from './ts-bloc.model';
 
@@ -32,7 +32,6 @@ export class TsMethod {
             const tsBloc: TsBloc = new TsBloc();
             tsBloc.node = this.node;
             tsBloc.depth = 0;
-            // this._tsBloc = TsBlocService.getBloc(tsBloc);œ
             this._tsBloc = Ast.getBloc(tsBloc);
             console.log('TS BLOCS children', this._tsBloc.children[6]);
         }
@@ -41,84 +40,12 @@ export class TsMethod {
 
     private evaluate(): Evaluation {
         const evaluation: Evaluation = new Evaluation();
-        evaluation.cognitiveValue = this.calculateCognitiveComplexity(this.node);
-        evaluation.cyclomaticValue = this.calculateCyclomaticComplexity(this.node);
+        evaluation.cognitiveValue = CS.calculateCognitiveComplexity(this.node);
+        evaluation.cyclomaticValue = CS.calculateCyclomaticComplexity(this.node);
         evaluation.methodName = Ast.getMethodName(this.node);
         evaluation.filename = this.tsFile?.sourceFile?.fileName ?? '';
         this._evaluation = evaluation;
         return evaluation;
-    }
-
-
-    calculateCognitiveComplexity(ctx): number {
-        let complexity = 0;
-        let depthLevel = 0;
-        let __self = this;
-        ts.forEachChild(ctx, function cb(node) {
-            if (utils.isFunctionWithBody(node)) {
-                depthLevel ++;
-                complexity += depthLevel;
-                ts.forEachChild(node, cb);
-            } else {
-                if (__self.increasesComplexity(node, 'cognitive')) {
-                    depthLevel ++;
-                    complexity += depthLevel;
-                }
-                ts.forEachChild(node, cb);
-            }
-        });
-        return complexity;
-    }
-
-
-    /**
-     * Calculates the cyclomatic complexity of a method
-     * @param node: ts.Node
-     */
-    calculateCyclomaticComplexity(node: ts.Node): number {
-        let totalComplexity = 1;
-        let __self = this;
-        ts.forEachChild(node, function cb(node) {
-            if (utils.isFunctionWithBody(node)) {
-                totalComplexity += 1;
-                ts.forEachChild(node, cb);
-            } else {
-                if (__self.increasesComplexity(node, 'cyclomatic')) {
-                    totalComplexity += 1;
-                }
-                ts.forEachChild(node, cb);
-            }
-        });
-        return totalComplexity;
-    }
-
-
-    increasesComplexity(node, method : 'cognitive' | 'cyclomatic') {
-        switch (node.kind) {
-            case ts.SyntaxKind.CaseClause:
-                return (node).statements.length > 0;
-            case ts.SyntaxKind.QuestionDotToken:
-                return method === 'cyclomatic';
-            case ts.SyntaxKind.CatchClause:
-            case ts.SyntaxKind.ConditionalExpression:
-            case ts.SyntaxKind.DoStatement:
-            case ts.SyntaxKind.ForStatement:
-            case ts.SyntaxKind.ForInStatement:
-            case ts.SyntaxKind.ForOfStatement:
-            case ts.SyntaxKind.IfStatement:
-            case ts.SyntaxKind.WhileStatement:
-                return true;
-            case ts.SyntaxKind.BinaryExpression:
-                switch ((node).operatorToken.kind) {
-                    case ts.SyntaxKind.BarBarToken:
-                    case ts.SyntaxKind.AmpersandAmpersandToken:
-                        return true;
-                    default:
-                        return false;
-                }
-            default:
-                return false;
-        }
     }
 
 }
--- src/services/ast.service.ts
+++ src/services/ast.service.ts
@@ -21,19 +21,16 @@ export class Ast {
         tree.syntaxKindName = Ast.getSyntaxKindName(tree.node);
         const depth: number = isBloc ? tree.depth : undefined;
         ts.forEachChild(tree.node, (childNode: ts.Node) => {
-            console.log('DEPTH', tree.depth);
             const newTree = isBloc ? new TsBloc() : new TsTree() as TsBloc;
             childNode.parent = tree.node;
             newTree.node = childNode;
             if (isBloc) {
-                newTree.depth = depth + 1;
-                console.log('NEW DEPTH', newTree.depth);
+                newTree.depth = Ast.increaseDepth(childNode, depth);
                 newTree.tsMethod = tree.tsMethod;
             }
             const childTree = this.parseChildNodes(newTree, isBloc);
             tree.children.push(childTree);
         });
-        // tree.node = node;
         return tree;
     }
 
@@ -45,7 +42,7 @@ export class Ast {
 
     static getMethodName(node: ts.Node): string {
         if (node?.kind === ts.SyntaxKind.MethodDeclaration) {
-                return node?.['name']?.['escapedText'] ?? '';
+            return node?.['name']?.['escapedText'] ?? '';
         } else {
             return '';
         }
@@ -54,5 +51,20 @@ export class Ast {
 
     static getPreviousNode(node: ts.Node, parentNode: ts.Node): ts.Node {
         return node;
+    }
+
+
+    static increaseDepth(node: ts.Node, depth: number): number {
+        let newDepth: number;
+        switch (node?.kind) {
+            case ts.SyntaxKind.IfStatement:
+                newDepth = depth + 1;
+                break;
+            default:
+                newDepth = depth;
+                break;
+        }
+        console.log(`NODE ${ts.SyntaxKind[node?.kind]} depth ${newDepth}`);
+        return newDepth;
     }
 }
