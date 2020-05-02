b300ca9538b418823a7157e3402d18fcda468ecd
Gilles Fabre <gilles.fabre@altran.com>
2020-05-02 10:43:47 +0200

    Cyclomatic report runs

--- src/models/ts-method.ts
+++ src/models/ts-method.ts
@@ -1,6 +1,5 @@
 import * as ts from 'typescript';
 import * as utils from 'tsutils';
-import { increasesComplexity } from '../services/complexity.service';
 import { TsFile } from './ts-file';
 import { TsTree } from './ts-tree.model';
 import { Ast } from '../services/ast.service';
@@ -28,7 +27,7 @@ export class TsMethod {
     private evaluate(): Evaluation {
         const evaluation: Evaluation = new Evaluation();
         evaluation.cognitiveValue = this.calculateCognitiveComplexity(this.node);
-        evaluation.cyclomaticValue = this.calculateCognitiveComplexity(this.node);
+        evaluation.cyclomaticValue = this.calculateCyclomaticComplexity(this.node);
         evaluation.methodName = Ast.getMethodName(this.node);
         evaluation.filename = this.tsFile?.sourceFile?.fileName ?? '';
         this._evaluation = evaluation;
@@ -39,13 +38,14 @@ export class TsMethod {
     calculateCognitiveComplexity(ctx): number {
         let complexity = 0;
         let depthLevel = 0;
+        let __self = this;
         ts.forEachChild(ctx, function cb(node) {
             if (utils.isFunctionWithBody(node)) {
                 depthLevel ++;
                 complexity += depthLevel;
                 ts.forEachChild(node, cb);
             } else {
-                if (increasesComplexity(node, 'cognitive')) {
+                if (__self.increasesComplexity(node, 'cognitive')) {
                     depthLevel ++;
                     complexity += depthLevel;
                 }
@@ -53,6 +53,57 @@ export class TsMethod {
             }
         });
         return complexity;
+    }
+
+
+    /**
+     * Calculates the cyclomatic complexity of a method
+     * @param node: ts.Node
+     */
+    calculateCyclomaticComplexity(node: ts.Node): number {
+        let totalComplexity = 1;
+        let __self = this;
+        ts.forEachChild(node, function cb(node) {
+            if (utils.isFunctionWithBody(node)) {
+                totalComplexity += 1;
+                ts.forEachChild(node, cb);
+            } else {
+                if (__self.increasesComplexity(node, 'cyclomatic')) {
+                    totalComplexity += 1;
+                }
+                ts.forEachChild(node, cb);
+            }
+        });
+        return totalComplexity;
+    }
+
+
+    increasesComplexity(node, method : 'cognitive' | 'cyclomatic') {
+        switch (node.kind) {
+            case ts.SyntaxKind.CaseClause:
+                return (node).statements.length > 0;
+            case ts.SyntaxKind.QuestionDotToken:
+                return method === 'cyclomatic';
+            case ts.SyntaxKind.CatchClause:
+            case ts.SyntaxKind.ConditionalExpression:
+            case ts.SyntaxKind.DoStatement:
+            case ts.SyntaxKind.ForStatement:
+            case ts.SyntaxKind.ForInStatement:
+            case ts.SyntaxKind.ForOfStatement:
+            case ts.SyntaxKind.IfStatement:
+            case ts.SyntaxKind.WhileStatement:
+                return true;
+            case ts.SyntaxKind.BinaryExpression:
+                switch ((node).operatorToken.kind) {
+                    case ts.SyntaxKind.BarBarToken:
+                    case ts.SyntaxKind.AmpersandAmpersandToken:
+                        return true;
+                    default:
+                        return false;
+                }
+            default:
+                return false;
+        }
     }
 
 }
--- src/services/complexity.service.ts
+++ src/services/complexity.service.ts
@@ -1,76 +1,28 @@
-import * as ts from 'typescript';
-import * as utils from 'tsutils';
-
-/**
- * Calculates the cognitive complexity of a method
- * @param method: ts.Node
- */
-export function getMethodCognitiveComplexity(method: ts.Node): number {
-    let complexity = 0;
-    let depthLevel = 0;
-    ts.forEachChild(method, function cb(node) {
-        if (utils.isFunctionWithBody(node)) {
-            depthLevel ++;
-            complexity += depthLevel;
-            ts.forEachChild(node, cb);
-        } else {
-            if (increasesComplexity(node, 'cognitive')) {
-                depthLevel ++;
-                complexity += depthLevel;
-            }
-            ts.forEachChild(node, cb);
-        }
-    });
-    return complexity;
-}
-
-
-
-/**
- * Calculates the cyclomatic complexity of a method
- * @param node: ts.Node
- */
-export function getMethodCyclomaticComplexity(node: ts.Node): number {
-    let totalComplexity = 1;
-    ts.forEachChild(node, function cb(node) {
-        if (utils.isFunctionWithBody(node)) {
-            totalComplexity += 1;
-            ts.forEachChild(node, cb);
-        } else {
-            if (increasesComplexity(node, 'cyclomatic')) {
-                totalComplexity += 1;
-            }
-            ts.forEachChild(node, cb);
-        }
-    });
-    return totalComplexity;
-}
-
-
-export function increasesComplexity(node, method : 'cognitive' | 'cyclomatic') {
-    switch (node.kind) {
-        case ts.SyntaxKind.CaseClause:
-            return (node).statements.length > 0;
-        case ts.SyntaxKind.QuestionDotToken:
-            return method === 'cyclomatic';
-        case ts.SyntaxKind.CatchClause:
-        case ts.SyntaxKind.ConditionalExpression:
-        case ts.SyntaxKind.DoStatement:
-        case ts.SyntaxKind.ForStatement:
-        case ts.SyntaxKind.ForInStatement:
-        case ts.SyntaxKind.ForOfStatement:
-        case ts.SyntaxKind.IfStatement:
-        case ts.SyntaxKind.WhileStatement:
-            return true;
-        case ts.SyntaxKind.BinaryExpression:
-            switch ((node).operatorToken.kind) {
-                case ts.SyntaxKind.BarBarToken:
-                case ts.SyntaxKind.AmpersandAmpersandToken:
-                    return true;
-                default:
-                    return false;
-            }
-        default:
-            return false;
-    }
-}
+// import * as ts from 'typescript';
+// import * as utils from 'tsutils';
+//
+// /**
+//  * Calculates the cognitive complexity of a method
+//  * @param method: ts.Node
+//  */
+// export function getMethodCognitiveComplexity(method: ts.Node): number {
+//     let complexity = 0;
+//     let depthLevel = 0;
+//     ts.forEachChild(method, function cb(node) {
+//         if (utils.isFunctionWithBody(node)) {
+//             depthLevel ++;
+//             complexity += depthLevel;
+//             ts.forEachChild(node, cb);
+//         } else {
+//             if (increasesComplexity(node, 'cognitive')) {
+//                 depthLevel ++;
+//                 complexity += depthLevel;
+//             }
+//             ts.forEachChild(node, cb);
+//         }
+//     });
+//     return complexity;
+// }
+//
+//
+// //
